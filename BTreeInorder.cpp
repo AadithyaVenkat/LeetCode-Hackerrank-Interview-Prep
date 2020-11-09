40cbfe2a4997065f3e0829531b69e5c9c4477a20
AadithyaVenkat <aadithyavenkat@gmail.com>
2020-11-08 18:03:20 -0800

    Create BTreeInorder.cpp

---
+++ BTreeInorder.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:    
+    void inorder(TreeNode* root, vector<int>& a)
+    {          
+        if (!root) {
+            return;
+        }
+        inorder(root->left,a);
+        a.push_back(root->val);
+        inorder(root->right,a);
+        
+    }
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        
+        vector<int> a;
+        inorder(root,a);
+        return a;
+       
+        
+    }
+};
