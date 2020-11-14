c31d275711e9bcc9fec4926f872482fbc072b404
AadithyaVenkat <aadithyavenkat@gmail.com>
2020-11-14 11:34:38 -0800

    Update LinkedListCycle.cpp

--- LinkedListCycle.cpp
+++ LinkedListCycle.cpp
@@ -1,21 +1,20 @@
-#Twopointer method fast and slow
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
      
-        if(head==NULL)
-            return false;
+       if(head==NULL)
+           return false;
         
         ListNode* fast=head;
         ListNode* slow=head;
-        while(fast->next && fast->next->next)
+        while(fast && slow && fast->next)
         {
-            if(slow==fast)
-                return true;
             fast=fast->next->next;
             slow=slow->next;
-        
+            if(slow==fast)
+                return true;
         }
+        
         return false;
         
     }
