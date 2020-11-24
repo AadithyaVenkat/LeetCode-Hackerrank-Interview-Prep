bf7622ac7c76c2b62c2cbe001cae8f14b0645523
AadithyaVenkat <aadithyavenkat@gmail.com>
2020-11-23 23:34:55 -0800

    Create FindTheDifference.cpp

---
+++ FindTheDifference.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        
+        char diff=0;
+        
+        for(int i=0;i<s.size();i++)
+            diff^=s[i];
+        for(int i=0;i<t.size();i++)
+            diff^=t[i];
+     
+        return diff;
+    }
+};
