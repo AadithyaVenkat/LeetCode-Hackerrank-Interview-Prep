46789b17057ad8051e4d9484ece538368f451831
AadithyaVenkat <aadithyavenkat@gmail.com>
2020-11-25 00:06:06 -0800

    Create MeetingRoom2.cpp

---
+++ MeetingRoom2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+        
+            
+        if(intervals.empty())
+            return 0;
+        
+        priority_queue<int, vector<int>, greater<int>> pq;
+        sort(intervals.begin(), intervals.end());
+        
+        pq.push(intervals[0][1]);
+        
+        for(int i=1; i<intervals.size(); i++)
+        {
+            if(intervals[i][0]>=pq.top())
+            {
+                pq.pop();
+                pq.push(intervals[i][1]);
+            }
+            else
+                pq.push(intervals[i][1]);
+        }
+        
+        return pq.size();
+        
+    }
+};
