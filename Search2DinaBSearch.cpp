36a9d591f618dc48bf04d2b69b0ee757db4c298f
AadithyaVenkat <aadithyavenkat@gmail.com>
2021-02-20 11:42:04 -0800

    Update Search2DinaBSearch.cpp

--- Search2DinaBSearch.cpp
+++ Search2DinaBSearch.cpp
@@ -3,19 +3,21 @@ public:
     bool searchMatrix(vector<vector<int>>& nums, int target) 
     {
         
-        for(int i=0;i<nums.size(); i++)
+        int m=nums.size(), n=nums[0].size();
+        if(m==0)
+            return 0;
+        int left=0, right=m*n-1;
+        
+        while(left<=right)
         {
-            int low=0, high=nums[i].size()-1;
-            while(low<=high)
-            {
-                int mid = (low+high)/2;
-                if(nums[i][mid]==target)
-                    return true;
-                else if(nums[i][mid]>target)
-                    high=mid-1;
-                else
-                    low=mid+1;
-            }
+            int mid=left+(right-left)/2;
+            int element=nums[mid/n][mid%n];
+            if(target==element)
+                return true;
+            else if(target<element)
+                right=mid-1;
+            else
+                left=mid+1;
         }
         
         return false;
